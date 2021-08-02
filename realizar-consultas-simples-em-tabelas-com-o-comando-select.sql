c9f39282939dafeb0253db5e985b4902380df0ee
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-08-01 23:16:50 -0300

    Banco de Dados com PostgreSQL
    Realizar consultas simples em tabelas com o comando select.

---
+++ realizar-consultas-simples-em-tabelas-com-o-comando-select.sql
@@ -0,0 +1,25 @@
+/**
+REIS, Fábio Dos. Postgresql: Como realizar consultas simples em tabelas com o comando SELECT. 
+10 sep. 2020, 07 jul. 2021. Notas de Aula. No prelo.
+*/
+
+SELECT * FROM clientes; 
+
+SELECT nome_cliente FROM clientes;
+
+SELECT nome_cliente, sobrenome_cliente FROM clientes;
+
+SELECT sobrenome_cliente, nome_cliente FROM clientes;
+
+SELECT * FROM produtos;
+
+SELECT nome_produto FROM produtos;
+
+SELECT nome_produto, descricao FROM produtos;
+
+SELECT nome_produto, preco, descricao 
+FROM produtos;
+
+SELECT *
+FROM pedidos;
+
