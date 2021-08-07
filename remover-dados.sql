ac4cf5a49decfd048313355ced8176a3c63f3e27
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-08-07 14:58:21 -0300

    Removendo dados no PostgreSQL
    BANCO de dados com postgresql: #07 - Removendo dados - Delete. Dirção de Descompila. Produção de Descompila. S. L.: Youtube, 2018. 
    7 videoaula (1 mins. 54 segs.), color.

---
+++ remover-dados.sql
@@ -0,0 +1,7 @@
+/**
+BANCO de dados com postgresql: #07 - Removendo dados - Delete. Dirção de Descompila. Produção de Descompila. S. L.: Youtube, 2018. 
+7 videoaula (1 mins. 54 segs.), color.
+*/
+
+-- Remover dados
+DELETE FROM categoria WHERE ID = 2
