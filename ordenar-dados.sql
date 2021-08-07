5ed3aed9f01b448efae233e45b7919a55bc012f2
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-08-07 15:33:58 -0300

    Ordenando dados no PostgreSQL
    BANCO de dados com postgresql: #09 - Ordenando dados - order by. Direção de Descompila. Produção de Descompila. S. L.: Youtube, 2018. 9 videoaula (2 mins. 11 segs.), color.

---
+++ ordenar-dados.sql
@@ -0,0 +1,15 @@
+/**
+BANCO de dados com postgresql: #09 - Ordenando dados - order by. Direção de Descompila. Produção de Descompila. S. L.: Youtube, 2018. 9 videoaula (2 mins. 11 segs.), color.
+*/
+
+-- 1ª ordenação de dados
+SELECT * FROM categoria 
+
+-- 2ª ordenação de dados
+SELECT * FROM categoria ORDER BY id
+
+-- 3ª ordenação de dados 
+SELECT * FROM categoria ORDER BY id ASC
+
+-- 4ª ordenação de dados
+SELECT * FROM categoria ORDER BY id DESC
