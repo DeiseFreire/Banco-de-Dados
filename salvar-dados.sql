8e033cec77f3b5cdb4167d43a590aa1fbde6defd
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-08-07 14:52:47 -0300

    Salvando dados no PostgreSQL
    BANCO de dados com postgresql: #05 - Salvando dados - Insert. Direção de Descompila. Produção de Descompila. S. L.: Youtube, 2018. 5 outros (2 mins. 49 segs.), color.

---
+++ salvar-dados.sql
@@ -0,0 +1,16 @@
+/**
+BANCO de dados com postgresql: #05 - Salvando dados - Insert. Direção de Descompila. Produção de Descompila. S. L.: Youtube, 2018. 5 videoaula (2 mins. 49 segs.), color.
+*/
+
+INSERT INTO categoria (descricao) VALUES ('Bebidas')
+INSERT INTO categoria (descricao) VALUES ('Comidas')
+INSERT INTO categoria (descricao) VALUES ('Teste')
+INSERT INTO categoria (descricao) VALUES ('Doces')
+INSERT INTO categoria (descricao) VALUES ('Hortifruti')
+INSERT INTO categoria (descricao) VALUES ('Limpeza')
+INSERT INTO categoria (descricao) VALUES ('Higiene')
+INSERT INTO categoria (descricao) VALUES ('Massas')
+INSERT INTO categoria (descricao) VALUES ('Laticínios')
+INSERT INTO categoria (descricao) VALUES ('Cereais')
+INSERT INTO categoria (descricao) VALUES ('Congelados')
+INSERT INTO categoria (descricao) VALUES ('Carnes')
